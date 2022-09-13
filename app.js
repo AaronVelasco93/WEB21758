624e121262ca193b12e3af83813f93cb24594739
AaronVelasco93 <aaronvelasco1933@outlook.com>
2022-09-13 16:33:41 -0500

    Clase 1 de JS

---
+++ app.js
@@ -0,0 +1,17 @@
+//Esta es una funcion
+function despedir(){
+    alert("Adios desde un archivo externo");
+}
+function mostrar(){
+    alert("desde mi terminal 2");
+}
+
+console.log("hola desde la consola :3");
+
+console.log("Este es un texto");
+console.log(20);
+console.log(2.6);
+console.log(1,63);
+console.log(1+2);
+console.log(true);
+console.log(false);
