a8c8987162131b6f792c603b6b5c2be285e7b53f
AaronVelasco93 <aaronvelasco1933@outlook.com>
2022-09-20 16:36:37 -0500

    Concatenacion y parseo

---
+++ Clase2/app.js
@@ -0,0 +1,22 @@
+//console.log("Hola mundo");
+//declaramos una variable con un valor inicial pero la podemos renombrar
+var nombre ="Aaron";
+var nombre="Gema";
+console.log(nombre);
+
+let user= "HuronMarron";
+//let user="Huron";
+console.log(user);
+const pas="123Aa";
+let nombreUsuario="Aaron";
+let edad=28;
+let nacionalidad="Mexincana";
+let compromiso = false;
+console.log("Su nombre es: ");
+console.log(nombreUsuario);
+console.log("su edad es: ");
+console.log(edad);
+console.log("Su nacionalidad es:");
+console.log(nacionalidad);
+console.log("¿Esta comprometid@ ?")
+console.log(compromiso);
---
+++ Clase2/app2.js
@@ -0,0 +1,17 @@
+var one = "hola";
+var dos = " como estas";
+var joined = one + dos;
+console.log(joined);
+
+var a= "Aaron";
+var b= "Velasco";
+console.log(a+b);
+console.log("Aaron "+"Velasco");
+
+let nombre="Aaron";
+let edad=28;
+console.log("Su nombre es: "+nombre+" Su edad es:"+edad);
+
+let numero1= 10;
+let numero2= 20;
+console.log(numero1+numero2);
